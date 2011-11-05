3edc9c98cd18629342709e8e811d03c521caf7a5
Yin <y@simande.com>
2011-11-05 12:48:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eatpager::Application.config.secret_token = '7779479c72dd0ec99be74d623b0608889da62b6933d974c26e228e6f818583da4f0c07a129ddb027c8b87857fb940b884fce71e10638c5dcd7cad20188c3e3b3'
