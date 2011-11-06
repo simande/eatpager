ce17c8be492e13de4d31c88c18d04c0ffd21cb7e
Eric <e@simande.com>
2011-11-05 20:52:12 -0400

    tips

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,14 +9,16 @@ class RestaurantsController < ApplicationController
     
     page = params[:page].to_i || 0
     
-    nearby = foursquare.venues.nearby(:ll => latlng, :category_id => "4d4b7105d754a06374d81259", :radius => 800)
+    nearby = foursquare.venues.nearby(:ll => latlng, :category_id => "4d4b7105d754a06374d81259", :radius => 160)
     page = page % nearby.length
     
-    @restaurant = nearby[page]
+    @restaurant = foursquare.venues.find(nearby[page].id)
     
     @distance_away = geo.distance_from(@restaurant.location.lat.to_s + "," + @restaurant.location.lng.to_s)
     
-    # OrdrIn::API.new("KtdDpAMI4RGuIUxbu8bTaA", "http://localhost:3000") # developer key and site where hosted
+    OrdrIn::API.new("KtdDpAMI4RGuIUxbu8bTaA", "http://localhost:3000") # developer key and site where hosted
+    place = OrdrIn::Address.new("1 Main St", "College Station", "77840", "Suite 200", "Texas", "4044099661", "Home") # street, city, zip, street2, state, phone, nickname
+    puts place
     
     open_data = OpenData.where('phone = ? and grade <> ?', @restaurant.contact["phone"], '').first
     
